a2a7118f24af42f99f1ead6178297d11bf38028c
Ronny Chan <ronny6993@gmail.com>
2018-05-17 12:19:56 -0400

    add appbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,6 +12,9 @@ import { withStyles, StyleRules } from '@material-ui/core/styles'
 import { StyleProps } from 'support/InjectSheet'
 import { SET_SELECTED_PLATFORM } from 'remoting/resolvers'
 import { Switch, Route } from 'react-router'
+import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core'
+import { Menu as MenuIcon } from '@material-ui/icons'
+import Sidebar from 'components/Sidebar/Sidebar'
 const PlatformViewer = withPlatforms(({ platforms }) => (
   <div>
     Hello
@@ -63,9 +66,11 @@ const StatefulPlatformSelector = withStyles(StatefulPlatformSelectorStyles)(
               <PlatformSelector
                 platforms={this.props.platforms}
                 selectedPlatform={this.props.selectedPlatform}
-                onSelectedPlatformChanged={({nextPlatform}) => {
-                  setSelectedPlatform({variables: { platformID: nextPlatform.PlatformID } })
-                //  this.setState({ selectedPlatform: nextPlatform })
+                onSelectedPlatformChanged={({ nextPlatform }) => {
+                  setSelectedPlatform({
+                    variables: { platformID: nextPlatform.PlatformID }
+                  })
+                  //  this.setState({ selectedPlatform: nextPlatform })
                 }}
                 gameCount={0}
               />
@@ -83,25 +88,44 @@ const appStyles: StyleRules = {
   app: {
     display: 'grid',
     height: '100%',
-    gridTemplateRows: 'auto 64px'
+    gridTemplateRows: '64px auto 64px',
+    gridTemplateColumns: '64px auto',
+    gridTemplateAreas: `
+                        "appbar appbar"
+                        "sidebar content"
+                        "bottombar bottombar"
+                        `
+    // gridTemplate: `"[sidebar] 64px span 2 [appbar] 64px"
+    //                "[content] auto"
+    //               `
+  },
+  appbar: {
+    gridArea: 'appbar'
   },
   platformSelectorContainer: {
-    gridArea: 2,
+    gridArea: 'bottombar',
     bottom: 0
   },
   content: {
-    gridArea: 1
+    gridArea: 'content'
+  },
+  appbarColor: {
+    background: 'white'
+  },
+  appbarInner: {
+    boxShadow: 'none',
+    borderBottom: '1px solid #eee'
   }
+  
 }
 
-const StatefulPlatformSelectorView = ({classes}) => {
+const StatefulPlatformSelectorView = ({ classes }) => {
   return (
     <React.Fragment>
-      <div className={classes.content}>
-        <div>Hello World</div>
-      </div>
-      <div className={classes.platformSelectorContainer}>
-          <StatefulPlatformSelector />
+      <div className={classes.gameGrid}>
+        <div className={classes.content}>
+          <div>Hello World</div>
+        </div>
       </div>
     </React.Fragment>
   )
@@ -109,11 +133,48 @@ const StatefulPlatformSelectorView = ({classes}) => {
 class App extends React.Component<StyleProps> {
   render() {
     return (
-        <div className={this.props.classes.app}>
+      <div className={this.props.classes.app}>
+        <div className={this.props.classes.sidebar}>
+          <Sidebar/>
+        </div>
+        <div className={this.props.classes.appbar}>
+        <AppBar
+          position="static"
+          color="default"
+          classes={{
+            colorDefault: this.props.classes.appbarColor,
+            positionSticky: this.props.classes.appbarSticky,
+            root: this.props.classes.appbarInner
+          }}
+        >
+          <Toolbar>
+            <IconButton className={this.props.classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit">
+              Snowflake
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        </div>
+        <div className={this.props.classes.content}>
           <Switch>
-            <Route path="/" render={(props) => <StatefulPlatformSelectorView {...this.props}/>}/>
+            <Route
+              path="/"
+              render={props => <div>Hello World</div>}
+            />
           </Switch>
         </div>
+        <Switch>
+          <Route
+            path="/"
+            render={props => <div className={this.props.classes.platformSelectorContainer}>
+                              <StatefulPlatformSelector />
+                    </div>}
+          />
+        </Switch>
+       
+      </div>
     )
   }
 }
